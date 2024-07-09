f4b3bc1cb902e1bb64b77086b79bb9b9882fc1f7
monkeysoop <istok.endre2@gmail.com>
2024-07-09 23:01:42 +0200

    added 2nd window to app

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -7,6 +8,99 @@
 
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
+
+
+void clean(SDL_Window* main_window, SDL_Renderer* main_renderer, SDL_Texture* main_texture, SDL_Window* debug_window, SDL_Renderer* debug_renderer, SDL_Texture* debug_texture) {
+    if (debug_window != NULL) {
+        SDL_DestroyTexture(debug_texture);
+    }
+    if (debug_renderer != NULL) {
+        SDL_DestroyRenderer(debug_renderer);
+    }
+    if (debug_texture != NULL) {
+        SDL_DestroyWindow(debug_window);
+    }
+    if (main_texture != NULL) {
+        SDL_DestroyTexture(main_texture);
+    }
+    if (main_renderer != NULL) {
+        SDL_DestroyRenderer(main_renderer);
+    }
+    if (main_window != NULL) {
+        SDL_DestroyWindow(main_window);
+    }
+}
+
+void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** main_texture, SDL_Window** debug_window, SDL_Renderer** debug_renderer, SDL_Texture** debug_texture) {
+    (*main_window) = SDL_CreateWindow(
+        "NES emulator",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        600, 
+        400,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+    );
+    if ((*main_window) == NULL) {
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Window creation] Error during the SDL initialization: %s", SDL_GetError());
+		exit(1);
+    }
+
+
+    (*main_renderer) = SDL_CreateRenderer((*main_window), -1, SDL_RENDERER_ACCELERATED);
+    if ((*main_renderer) == NULL) {
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Renderer creation] Error during the SDL initialization: %s", SDL_GetError());
+		exit(1);
+    }
+    
+    
+    (*main_texture) = SDL_CreateTexture(
+        (*main_renderer), 
+        SDL_PIXELFORMAT_RGBA8888, 
+        SDL_TEXTUREACCESS_TARGET, 
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT
+    );
+    if ((*main_texture) == NULL) {
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Texture creation] Error during the SDL initialization: %s", SDL_GetError());
+		exit(1);
+    }
+    
+    
+    (*debug_window) = SDL_CreateWindow(
+        "NES debug view",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        600, 
+        400,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+    );
+    if ((*debug_window) == NULL) {
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Debug window creation] Error during the SDL initialization: %s", SDL_GetError());
+		exit(1);
+    }
+    
+    
+    (*debug_renderer) = SDL_CreateRenderer((*debug_window), -1, SDL_RENDERER_ACCELERATED);
+    if ((*debug_renderer) == NULL) {
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Renderer creation] Error during the SDL initialization: %s", SDL_GetError());
+		exit(1);
+    }
+    
+    
+    (*debug_texture) = IMG_LoadTexture((*debug_renderer), "font.png");
+    if ((*debug_texture) == NULL) {
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Texture loading] Error during the SDL initialization: %s", SDL_GetError());
+		exit(1);
+    }
+}
+
 
 
 void render(uint32_t* pixels, Uint64 time) {
@@ -26,51 +120,31 @@ int main(int argc, char** argv)
 		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[SDL initialization] Error during the SDL initialization: %s", SDL_GetError());
 		return 1;
 	}
+    IMG_Init(IMG_INIT_PNG);
 
+    
+    SDL_Window* main_window = NULL;
+    SDL_Renderer* main_renderer = NULL;
+    SDL_Texture* main_texture = NULL;
+    SDL_Window* debug_window = NULL;
+    SDL_Renderer* debug_renderer = NULL;
+    SDL_Texture* debug_texture = NULL;
 
-    SDL_Window* window = SDL_CreateWindow(
-        "Basic C SDL project",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_WIDTH * 2, 
-        SCREEN_HEIGHT * 2,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
-    );
+    init(&main_window, &main_renderer, &main_texture, &debug_window, &debug_renderer, &debug_texture);
 
-    if (window == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Window creation] Error during the SDL initialization: %s", SDL_GetError());
-		return 1;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        SDL_DestroyWindow(window);
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Renderer creation] Error during the SDL initialization: %s", SDL_GetError());
-		return 1;
-    }
-
-    SDL_Rect squareRect;
-    squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-    squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-    squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-    squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
+    
 
     uint32_t* pixels = malloc(SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(uint32_t));
     memset(pixels, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(uint32_t));
 
 
-    SDL_Texture* texture = SDL_CreateTexture(
-        renderer, 
-        SDL_PIXELFORMAT_RGBA8888, 
-        SDL_TEXTUREACCESS_TARGET, 
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT
-    );
 
-
+    SDL_HideWindow(debug_window);
     Uint64 previous_time = SDL_GetTicks();
 	int frame_counter = 0;
+
+    bool debug_shown = false;
+    bool i_pressed = false;
 
     bool quit = false;
     while (!quit) {
@@ -82,13 +156,19 @@ int main(int argc, char** argv)
 					quit = true;
 					break;
 				case SDL_KEYDOWN:
-					if (ev.key.keysym.sym == SDLK_ESCAPE || ev.key.keysym.sym == SDLK_q) {
-						quit = true;
+                    switch (ev.key.keysym.sym) {
+                        case SDLK_ESCAPE: quit = true; break;
+                        case SDLK_q: quit = true; break;
+                        default: break;
                     }
 					//app.KeyboardDown(ev.key);
 					break;
 				case SDL_KEYUP:
-					//app.KeyboardUp(ev.key);
+					switch (ev.key.keysym.sym) {
+                        case SDLK_i: i_pressed = true; break;
+                        default: break;
+                    }
+                    //app.KeyboardUp(ev.key);
 					break;
 				case SDL_MOUSEBUTTONDOWN:
 					//app.MouseDown(ev.button);
@@ -117,23 +197,29 @@ int main(int argc, char** argv)
 			}
 		}
 
-        //SDL_LockTexture(texture, NULL, NULL, NULL);
+        if (i_pressed) {
+            (debug_shown) ? SDL_HideWindow(debug_window) : SDL_ShowWindow(debug_window);
+            debug_shown = !(debug_shown);
+            i_pressed = false;
+        }
+
+
+
 		render(pixels, previous_time);
-        SDL_UpdateTexture(texture, NULL, pixels, SCREEN_WIDTH * sizeof(uint32_t));
-        //SDL_UnlockTexture(texture);
-
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-        SDL_RenderClear(renderer);
-
+        SDL_UpdateTexture(main_texture, NULL, pixels, SCREEN_WIDTH * sizeof(uint32_t));
+        
+        SDL_SetRenderDrawColor(main_renderer, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(main_renderer);
+        
         int w;
         int h;
-        SDL_GetRendererOutputSize(renderer, &w, &h);
-
+        SDL_GetRendererOutputSize(main_renderer, &w, &h);
+        
         SDL_Rect screen_rect = {.x=0, .y=0, .w=w, .h=h};
         
         SDL_RenderCopyEx(
-            renderer, 
-            texture, 
+            main_renderer, 
+            main_texture, 
             NULL, 
             &screen_rect,
             0.0,
@@ -141,8 +227,16 @@ int main(int argc, char** argv)
             SDL_FLIP_NONE
         );
         
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(main_renderer);
         
+
+        if (debug_shown) {
+            SDL_RenderCopy(debug_renderer, debug_texture, NULL, NULL);
+            SDL_RenderPresent(debug_renderer);
+        }
+
+
+
 
 		frame_counter++;
         Uint64 current_time = SDL_GetTicks64();
@@ -153,10 +247,8 @@ int main(int argc, char** argv)
 		}
 	}
 
-    SDL_DestroyTexture(texture);
     free(pixels);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    clean(main_window, main_renderer, main_texture, debug_window, debug_renderer, debug_texture);
     SDL_Quit();
     return 0;
 }
