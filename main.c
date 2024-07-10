04b3cdcc337f3f3907a286090a3741d05c3b1371
monkeysoop <istok.endre2@gmail.com>
2024-07-10 21:05:40 +0200

    character rendering

--- main.c
+++ main.c
@@ -3,15 +3,22 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define SCREEN_WIDTH 256
-#define SCREEN_HEIGHT 240
+#define NES_SCREEN_WIDTH 256
+#define NES_SCREEN_HEIGHT 240
 
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define FONT_CHAR_SIZE 8
+#define FONT_CHARS_WIDTH 16
+#define FONT_CHARS_HEIGHT 16
 
+#define CHARS_WIDTH 16
+#define CHARS_HEIGHT 16
 
+#define DEBUG_PADDING 10
 
-void clean(SDL_Window* main_window, SDL_Renderer* main_renderer, SDL_Texture* main_texture, SDL_Window* debug_window, SDL_Renderer* debug_renderer, SDL_Texture* debug_texture) {
+void clean(SDL_Window* main_window, SDL_Renderer* main_renderer, SDL_Texture* main_texture, SDL_Window* debug_window, SDL_Renderer* debug_renderer, SDL_Texture* debug_texture, SDL_Texture* font_texture) {
+    if (font_texture != NULL) {
+        SDL_DestroyTexture(font_texture);
+    }
     if (debug_window != NULL) {
         SDL_DestroyTexture(debug_texture);
     }
@@ -32,7 +39,7 @@ void clean(SDL_Window* main_window, SDL_Renderer* main_renderer, SDL_Texture* ma
     }
 }
 
-void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** main_texture, SDL_Window** debug_window, SDL_Renderer** debug_renderer, SDL_Texture** debug_texture) {
+void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** main_texture, SDL_Window** debug_window, SDL_Renderer** debug_renderer, SDL_Texture** debug_texture, SDL_Texture** font_texture) {
     (*main_window) = SDL_CreateWindow(
         "NES emulator",
         SDL_WINDOWPOS_UNDEFINED,
@@ -42,7 +49,7 @@ void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** 
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
     if ((*main_window) == NULL) {
-        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture), (*font_texture));
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Window creation] Error during the SDL initialization: %s", SDL_GetError());
 		exit(1);
     }
@@ -50,7 +57,7 @@ void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** 
 
     (*main_renderer) = SDL_CreateRenderer((*main_window), -1, SDL_RENDERER_ACCELERATED);
     if ((*main_renderer) == NULL) {
-        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture), (*font_texture));
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Renderer creation] Error during the SDL initialization: %s", SDL_GetError());
 		exit(1);
     }
@@ -60,11 +67,11 @@ void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** 
         (*main_renderer), 
         SDL_PIXELFORMAT_RGBA8888, 
         SDL_TEXTUREACCESS_TARGET, 
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT
+        NES_SCREEN_WIDTH,
+        NES_SCREEN_HEIGHT
     );
     if ((*main_texture) == NULL) {
-        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture), (*font_texture));
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Texture creation] Error during the SDL initialization: %s", SDL_GetError());
 		exit(1);
     }
@@ -74,12 +81,12 @@ void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** 
         "NES debug view",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        600, 
-        400,
+        CHARS_WIDTH * FONT_CHAR_SIZE * 2, 
+        CHARS_HEIGHT * FONT_CHAR_SIZE * 2,
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
     if ((*debug_window) == NULL) {
-        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture), (*font_texture));
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Debug window creation] Error during the SDL initialization: %s", SDL_GetError());
 		exit(1);
     }
@@ -87,26 +94,106 @@ void init(SDL_Window** main_window, SDL_Renderer** main_renderer, SDL_Texture** 
     
     (*debug_renderer) = SDL_CreateRenderer((*debug_window), -1, SDL_RENDERER_ACCELERATED);
     if ((*debug_renderer) == NULL) {
-        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture), (*font_texture));
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Renderer creation] Error during the SDL initialization: %s", SDL_GetError());
 		exit(1);
     }
     
-    
-    (*debug_texture) = IMG_LoadTexture((*debug_renderer), "font.png");
+
+    (*debug_texture) = SDL_CreateTexture(
+        (*debug_renderer), 
+        SDL_PIXELFORMAT_RGBA8888, 
+        SDL_TEXTUREACCESS_TARGET, 
+        CHARS_WIDTH * FONT_CHAR_SIZE,
+        CHARS_HEIGHT * FONT_CHAR_SIZE
+    );
     if ((*debug_texture) == NULL) {
-        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture));
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture), (*font_texture));
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Texture loading] Error during the SDL initialization: %s", SDL_GetError());
+		exit(1);
+    }
+
+    
+    (*font_texture) = IMG_LoadTexture((*debug_renderer), "font.png");
+    if ((*font_texture) == NULL) {
+        clean((*main_window), (*main_renderer), (*main_texture), (*debug_window), (*debug_renderer), (*debug_texture), (*font_texture));
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[Texture loading] Error during the SDL initialization: %s", SDL_GetError());
 		exit(1);
     }
 }
 
 
+void main_render(SDL_Renderer* main_renderer, SDL_Texture* main_texture, uint32_t* pixels) {
+    SDL_UpdateTexture(main_texture, NULL, pixels, NES_SCREEN_WIDTH * sizeof(uint32_t));
+        
+    SDL_SetRenderDrawColor(main_renderer, 0x00, 0x00, 0x00, 0xFF);
+    SDL_RenderClear(main_renderer);
+    
+    
+    SDL_RenderCopyEx(
+        main_renderer, 
+        main_texture, 
+        NULL, 
+        NULL,
+        0.0,
+        NULL,
+        SDL_FLIP_NONE
+    );
+    
+    SDL_RenderPresent(main_renderer);
+}
 
-void render(uint32_t* pixels, Uint64 time) {
-    for (int y = 0; y < SCREEN_HEIGHT; y++) {
-        for (int x = 0; x < SCREEN_WIDTH; x++) {
-            pixels[y * SCREEN_WIDTH + x] = 0x000000FF | (((x / 8) + ((y / 8) % 2)) % 2) * 0x77000000; 
+
+void debug_render(SDL_Renderer* debug_renderer, SDL_Texture* debug_texture, SDL_Texture* font_texture, char* chars) {
+    SDL_SetRenderDrawColor(debug_renderer, 0x00, 0x00, 0x00, 0xFF);
+    SDL_RenderClear(debug_renderer);
+
+
+
+    SDL_SetRenderTarget(debug_renderer, debug_texture);
+
+    for (int y = 0; y < CHARS_HEIGHT; y++) {
+        for (int x = 0; x < CHARS_WIDTH; x++) {
+            uint8_t c = (uint8_t)chars[y * CHARS_WIDTH + x];
+
+            uint8_t c_x = c % CHARS_WIDTH;
+            uint8_t c_y = c / (uint8_t)CHARS_WIDTH;
+
+            SDL_Rect char_rect = {.x=(c_x * FONT_CHAR_SIZE), .y=(c_y * FONT_CHAR_SIZE), .w=FONT_CHAR_SIZE, .h=FONT_CHAR_SIZE};
+            SDL_Rect target_rect = {.x =(x * FONT_CHAR_SIZE), .y=(y * FONT_CHAR_SIZE), .w=FONT_CHAR_SIZE, .h=FONT_CHAR_SIZE};
+
+            SDL_RenderCopyEx(
+                debug_renderer, 
+                font_texture, 
+                &char_rect,
+                &target_rect,
+                0.0,
+                NULL,
+                SDL_FLIP_NONE
+            );
+        }
+    }
+    SDL_SetRenderTarget(debug_renderer, NULL);
+    
+    
+    SDL_RenderCopyEx(
+        debug_renderer, 
+        debug_texture, 
+        NULL, 
+        NULL,
+        0.0,
+        NULL,
+        SDL_FLIP_NONE
+    );
+
+    SDL_RenderPresent(debug_renderer);
+}
+
+
+void render(uint32_t* pixels) {
+    for (int y = 0; y < NES_SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < NES_SCREEN_WIDTH; x++) {
+            pixels[y * NES_SCREEN_WIDTH + x] = 0x000000FF | (((x / 8) + ((y / 8) % 2)) % 2) * 0xFF000000; 
         }
     }
 }
@@ -129,14 +216,18 @@ int main(int argc, char** argv)
     SDL_Window* debug_window = NULL;
     SDL_Renderer* debug_renderer = NULL;
     SDL_Texture* debug_texture = NULL;
+    SDL_Texture* font_texture = NULL;
 
-    init(&main_window, &main_renderer, &main_texture, &debug_window, &debug_renderer, &debug_texture);
+    init(&main_window, &main_renderer, &main_texture, &debug_window, &debug_renderer, &debug_texture, &font_texture);
 
     
 
-    uint32_t* pixels = malloc(SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(uint32_t));
-    memset(pixels, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(uint32_t));
+    uint32_t* pixels = malloc(NES_SCREEN_WIDTH * NES_SCREEN_HEIGHT * sizeof(uint32_t));
+    memset(pixels, 0, NES_SCREEN_WIDTH * NES_SCREEN_HEIGHT * sizeof(uint32_t));
 
+
+    char* chars = malloc(CHARS_WIDTH * CHARS_HEIGHT * sizeof(char));
+    memset(chars, '\0', CHARS_WIDTH * CHARS_HEIGHT * sizeof(char));
 
 
     SDL_HideWindow(debug_window);
@@ -205,34 +296,34 @@ int main(int argc, char** argv)
 
 
 
-		render(pixels, previous_time);
-        SDL_UpdateTexture(main_texture, NULL, pixels, SCREEN_WIDTH * sizeof(uint32_t));
-        
-        SDL_SetRenderDrawColor(main_renderer, 0xFF, 0x00, 0x00, 0xFF);
-        SDL_RenderClear(main_renderer);
-        
-        int w;
-        int h;
-        SDL_GetRendererOutputSize(main_renderer, &w, &h);
-        
-        SDL_Rect screen_rect = {.x=0, .y=0, .w=w, .h=h};
-        
-        SDL_RenderCopyEx(
-            main_renderer, 
-            main_texture, 
-            NULL, 
-            &screen_rect,
-            0.0,
-            NULL,
-            SDL_FLIP_NONE
-        );
-        
-        SDL_RenderPresent(main_renderer);
+		render(pixels);
+        main_render(main_renderer, main_texture, pixels);
+        //SDL_UpdateTexture(main_texture, NULL, pixels, NES_SCREEN_WIDTH * sizeof(uint32_t));
+        //
+        //SDL_SetRenderDrawColor(main_renderer, 0xFF, 0x00, 0x00, 0xFF);
+        //SDL_RenderClear(main_renderer);
+        //
+        //int w;
+        //int h;
+        //SDL_GetRendererOutputSize(main_renderer, &w, &h);
+        //
+        //SDL_Rect screen_rect = {.x=0, .y=0, .w=w, .h=h};
+        //
+        //SDL_RenderCopyEx(
+        //    main_renderer, 
+        //    main_texture, 
+        //    NULL, 
+        //    &screen_rect,
+        //    0.0,
+        //    NULL,
+        //    SDL_FLIP_NONE
+        //);
+        //
+        //SDL_RenderPresent(main_renderer);
         
 
         if (debug_shown) {
-            SDL_RenderCopy(debug_renderer, debug_texture, NULL, NULL);
-            SDL_RenderPresent(debug_renderer);
+            debug_render(debug_renderer, debug_texture, font_texture, chars);
         }
 
 
@@ -247,8 +338,9 @@ int main(int argc, char** argv)
 		}
 	}
 
+    free(chars);
     free(pixels);
-    clean(main_window, main_renderer, main_texture, debug_window, debug_renderer, debug_texture);
+    clean(main_window, main_renderer, main_texture, debug_window, debug_renderer, debug_texture, font_texture);
     SDL_Quit();
     return 0;
 }
