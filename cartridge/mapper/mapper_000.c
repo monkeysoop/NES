7845ff4582ead661d9f1236ffcb98e94caa8da68
monkeysoop <istok.endre2@gmail.com>
2024-07-19 15:40:46 +0200

    renaming functions

--- cartridge/mapper/mapper_000.c
+++ cartridge/mapper/mapper_000.c
@@ -1,51 +1,36 @@
-#include "mapper.h"
+#include "cartridge.h"
 
 
-void Mapper000Init(Mapper* mapper, uint8_t prg_rom_16KB_units, uint8_t chr_rom_8KB_units) {
-    size_t prg_rom_size = prg_rom_16KB_units * 16 * 1024 * sizeof(uint8_t);
-    size_t chr_rom_size = chr_rom_8KB_units * 16 * 1024 * sizeof(uint8_t);
-
+void Mapper000Init(Cartridge* cartridge, uint8_t prg_rom_16KB_units, uint8_t prg_ram_8KB_units, uint8_t chr_rom_8KB_units) {
     if (prg_rom_16KB_units == 1) {
-        mapper->cpu_address_mask = 0x7FFF;
+        cartridge->cpu_address_mask = 0x7FFF;
     } else {
-        mapper->cpu_address_mask = 0x3FFF;
+        cartridge->cpu_address_mask = 0x3FFF;
     }
 
-    mapper->prg_rom = malloc(prg_rom_size);
-    mapper->chr_rom = malloc(chr_rom_size);
+    cartridge->MapperReadCPU = &Mapper000ReadCPU;
+    cartridge->MapperReadPPU = &Mapper000ReadPPU;
+    cartridge->MapperWriteCPU = &Mapper000WriteCPU;
+    cartridge->MapperWritePPU = &Mapper000WritePPU;
 
-    memset(mapper->prg_rom, 0, prg_rom_size);
-    memset(mapper->chr_rom, 0, chr_rom_size);
+    cartridge->MapperScanlineIRQ = NULL;
+}
 
-    mapper->MapperReadCPU = &MapperReadCPU;
-    mapper->MapperReadPPU = &MapperReadPPU;
-    mapper->MapperWriteCPU = &MapperWriteCPU;
-    mapper->MapperWritePPU = &MapperWritePPU;
+uint8_t Mapper000ReadCPU(Cartridge* cartridge, uint16_t address) {
+    return (cartridge->prg_rom[(address - 0x8000) & cartridge->cpu_address_mask]);
+}
 
-    mapper->MapperClean = &MapperClean;
+uint8_t Mapper000ReadPPU(Cartridge* cartridge, uint16_t address) {
+    return cartridge->chr_rom[address];
 }
 
 
-void MapperClean(Mapper* mapper) {
-    free(mapper->prg_rom);
-    free(mapper->chr_rom);
-}
-
-uint8_t MapperReadCPU(struct Mapper* mapper, uint16_t address) {
-    return (mapper->prg_rom[(address - 0x8000) & mapper->cpu_address_mask]);
-}
-
-uint8_t MapperReadPPU(struct Mapper* mapper, uint16_t address) {
-    return mapper->chr_rom[address];
-}
-
-
-void MapperWriteCPU(struct Mapper* mapper, uint16_t address, uint8_t data) {
+void Mapper000WriteCPU(Cartridge* cartridge, uint16_t address, uint8_t data) {
     printf("Attempted write to prg rom on mapper 000\n");
     exit(1);
 }
 
-void MapperWritePPU(struct Mapper* mapper, uint16_t address, uint8_t data) {
+void Mapper000WritePPU(Cartridge* cartridge, uint16_t address, uint8_t data) {
     printf("Attempted write to chr rom on mapper 000 (some games can have chr rams but it's not supported)\n");
     exit(1);
 }
